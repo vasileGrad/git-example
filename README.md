7704d9bd400860a8d81bcad13e5c1f8a90aa7258
Grad Vasile <grad_simplu@yahoo.com>
2017-09-21 01:29:59 +0200

    fixes nasty bug

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ git-example
 A Symfony project created on September 20, 2017, 11:10 pm.
 
 a new line here
+
+add something
+different here
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,6 +15,8 @@ class DefaultController extends Controller
     {
         $someVar = true;
 
+        // hello world!
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
